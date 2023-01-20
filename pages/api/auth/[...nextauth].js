91a2f40f2716000560460cb30cd77774be327b5e
Shashwat <shashwat>
2023-01-20 21:00:08 +0530

    production: auth issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      secret: process.env.NEXTAUTH_SECRET,
     }),
     // ...add more providers here
   ],
